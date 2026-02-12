c2fdbc90b3de5901706f8aeee30b5cc060e78edc
LorisSuperstar <180442737+LorisSuperstar@users.noreply.github.com>
2026-02-12 13:59:12 +0100

    Make Media Struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
+
+enum MediaType {
+    Video,
+    Game,
+    Book,
+    Music,
+}
+
+struct Media {
+    name: String,
+    mediatype: MediaType,
+    finished: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let media_list: Vec<Media> = vec![];
+
+        println!("Hello, user!");
+        println!("what do you want to do in your Media libary??? (<help> for options)");
+
+        let mut user_input = String::new();
+        let stdin = io::stdin();
+        stdin.read_line(&mut user_input).unwrap();
+
+        let user_input = user_input.trim();
+        match user_input {
+            "help" => println!("hello World"),
+
+            _ => println!("Command doesnt exist"),
+        }
+    }
 }
